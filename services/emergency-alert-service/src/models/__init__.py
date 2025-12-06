0467360e8e811a452db02b2a23f1f405edc00eff
Asma Mestaysser <110032159+asmamest@users.noreply.github.com>
2025-12-06 11:59:08 +0100

    Delete services/emergency-alert-service directory

--- services/emergency-alert-service/src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Modèles de données
-"""
-from src.models.alert import Alert, Location, AlertType, AlertStatus, Priority
-
-__all__ = ['Alert', 'Location', 'AlertType', 'AlertStatus', 'Priority']
--- services/emergency-alert-service/src/repository/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Couche repository
-"""
-from src.repository.alert_repository import AlertRepository
-
-__all__ = ['AlertRepository']
